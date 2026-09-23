28f6b552439bf3d29e41af2aa4fdd253a6e1f72a
Prathamesh <prathameshaj2004@gmail.com>
2026-09-23 22:00:59 +0530

    Refactor loginUser function to return a unified token structure from the authentication response

--- services/authService.js
+++ services/authService.js
@@ -2,5 +2,10 @@ import axiosInstance from "@/lib/axios";
 
 export async function loginUser(credentials) {
   const response = await axiosInstance.post("/auth/login", credentials);
-  return response.data;
+  const data = response.data;
+
+  return {
+    ...data,
+    token: data.token || data.accessToken,
+  };
 }
